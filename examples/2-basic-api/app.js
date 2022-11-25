e39f4ccdb57833f419d3ddfd3f7318932201ac61
Michael Babin <info@micb.dev>
2022-11-25 06:37:27 -0500

    adding data

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,12 @@
 const result = document.querySelector('.result')
+
+const fetchData = async () => {
+    try{
+        const {data} = await axios.get('/api/2-basic-api')
+        console.log(data)
+    } catch(error) {
+        console.log(error.response.data)
+    }
+}
+
+fetchData();
